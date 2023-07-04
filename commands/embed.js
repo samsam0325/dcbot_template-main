67e7106feb7a00f7c9a6944bbd4f4140d620e471
samsam0325 <125185308+samsam0325@users.noreply.github.com>
2023-07-04 17:12:24 +0800

    finish dice function

---
+++ commands/embed.js
@@ -0,0 +1,19 @@
+const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
+module.exports = {
+    data: new SlashCommandBuilder().setName("embed").setDescription("show an embed"),
+    async execute(client, interaction) {
+        const embed = new EmbedBuilder().setTitle("I'm an Embed")
+        .setColor('Blue').setImage("https://janstockcoin.com/2021/06/19/no-copyright-image/");
+        interaction.reply({ embeds: [embed] });
+    },
+};
+/*
+.setURL('放URL')
+.setAuthor({ name: '', iconURL: '放URL', url: '放URL' })
+.setDescription('')
+.setThumbnail('放URL')
+.addFields({ name: '', value: '', inline: true or false })
+.setImage('放URL')
+.setTimestamp()
+.setFooter({ text: '', iconURL: '放URL' });
+*/
